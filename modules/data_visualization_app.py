bfb8b19dc03dc869feec78ae33ef0ce574c13eb4
stemtazoo <43175204+stemtazoo@users.noreply.github.com>
2024-10-23 23:18:16 +0900

    update

--- modules/data_visualization_app.py
+++ modules/data_visualization_app.py
@@ -1,10 +1,12 @@
 import json
 import os
+import time
+import importlib.resources as pkg_resources
 
 import streamlit as st
 import pandas as pd
 
-from modules.state_manager import initialize_session_state
+from modules.state_manager import initialize_session_state, reset_session_state_to_default, apply_user_settings
 from modules.ui_components import file_uploader, graph_type_selector
 from modules.sample_data import load_simple_sample_data, load_timeseries_sample_data
 from modules.file_loader import load_csv_with_dynamic_start
@@ -13,39 +15,73 @@ from modules.plot import create_plot
 from modules.utils import download_chart_html
 
 # 設定ファイルの読み込み
-def load_config(config_file_path='config/config.json'):
-    with open(config_file_path, 'r', encoding='utf-8') as f:
+def load_config():
+    with pkg_resources.open_text('config', 'config.json') as f:
         return json.load(f)
+    
+def get_user_settings_path():
+    # Windows の場合、ユーザーのドキュメントフォルダに保存する
+    if os.name == 'nt':
+        user_documents = os.path.join(os.path.expanduser('~'), 'Documents')
+    else:
+        # 他のシステム (Linux, macOS) の場合はホームディレクトリに保存する
+        user_documents = os.path.expanduser('~')
+        
+    # ユーザー設定ファイルの完全なパスを指定
+    user_settings_path = os.path.join(user_documents, 'user_settings.json')
+    
+    return user_settings_path
 
-# ユーザー設定の読み込み
-def load_user_settings(user_settings_file='config/user_settings.json'):
-    if os.path.exists(user_settings_file):
-        with open(user_settings_file, 'r', encoding='utf-8') as f:
-            return json.load(f)
+def save_user_settings(settings):
+    user_settings_path = get_user_settings_path()
+    
+    try:
+        with open(user_settings_path, 'w', encoding='utf-8') as f:
+            json.dump(settings, f, indent=4, ensure_ascii=False)
+            st.success("設定が保存されました！")
+    except Exception as e:
+        st.error(f"設定の保存に失敗しました: {e}")
+
+def load_user_settings():
+    user_settings_path = get_user_settings_path()
+
+    if os.path.exists(user_settings_path):
+        try:
+            with open(user_settings_path, 'r', encoding='utf-8') as f:
+                return json.load(f)
+        except Exception as e:
+            st.error(f"設定の読み込みに失敗しました: {e}")
+            return {}
     else:
         return {}
 
-# ユーザー設定の保存
-def save_user_settings(settings, user_settings_file='config/user_settings.json'):
-    with open(user_settings_file, 'w', encoding='utf-8') as f:
-        json.dump(settings, f, indent=4, ensure_ascii=False)
-        st.success("設定が保存されました！")
+def merge_settings(default_settings, user_settings):
+    # default_settingsの各キーに対して、user_settingsの値が存在する場合はそれを上書き
+    if len(user_settings) > 0:
+        for key, value in user_settings.items():
+            if key in default_settings and isinstance(value, type(default_settings[key])):
+                default_settings[key] = value
+    return default_settings
 
 config = load_config()
 user_settings = load_user_settings()
 
 def app():
-    # 状態を初期化（ユーザー設定を反映）
-    initial_state = config['default_settings']
-    initial_state.update(user_settings)
-    initialize_session_state(initial_state)
+    # セッション状態にリセットフラグを追加し、存在しない場合はデフォルトでFalseを設定
+    if 'reset_triggered' not in st.session_state:
+        st.session_state['reset_triggered'] = False
+    
+    if st.session_state['reset_triggered'] == False:
+        initial_state = merge_settings(config['default_settings'], user_settings)
+        initialize_session_state(initial_state)
+        st.session_state['reset_triggered'] = True
 
     # タイトルの表示
-    st.title(config['default_settings']['title'])
+    st.title(st.session_state['title'])
 
     # タブの作成
-    tab_titles = config['default_settings']['tab_titles']
-    tab1, tab2 = st.tabs(tab_titles)
+    tab_titles = st.session_state['tab_titles']
+    tab1, tab2, tab3 = st.tabs(tab_titles)
 
     with tab1:
         # ファイルアップロードのインターフェース
@@ -67,12 +103,13 @@ def app():
             except Exception as e:
                 st.error(f"ファイルの読み込み中にエラーが発生しました: {e}")
         else:
-            # サンプルデータの生成ボタン
-            use_sample_data = st.selectbox("サンプルデータの選択", ["単純データ", "時系列データ"])
-            if use_sample_data == "単純データ":
-                st.session_state['df_origin'] = load_simple_sample_data()
-            elif use_sample_data == "時系列データ":
-                st.session_state['df_origin'] = load_timeseries_sample_data()
+            if st.button('サンプルデータを使用する。', key='button_sample'):
+                # サンプルデータの生成
+                use_sample_data = st.selectbox("サンプルデータの選択", ["単純データ", "時系列データ"])
+                if use_sample_data == "単純データ":
+                    st.session_state['df_origin'] = load_simple_sample_data()
+                elif use_sample_data == "時系列データ":
+                    st.session_state['df_origin'] = load_timeseries_sample_data()
 
         if st.session_state['df_origin'] is not None:
             # フィルタの適用
@@ -99,18 +136,31 @@ def app():
                 y_axis = st.multiselect("Y軸を選択", df.columns, key="y_axis_selectbox")
 
             # 折れ線グラフの場合、第2軸の設定を追加
-            use_secondary_y = False
-            secondary_y_axis = []
-            primary_y_axis = []
-            if chart_type == "折れ線グラフ":
-                use_secondary_y = st.checkbox("第2軸（右側Y軸）を使用する", key="use_secondary_y_checkbox")
-                if use_secondary_y:
-                    primary_y_axis = st.multiselect("第1軸（左側Y軸）を選択", y_axis, key="primary_y_axis_multiselect")
-                    secondary_y_axis = st.multiselect("第2軸（右側Y軸）を選択", [col for col in y_axis if col not in primary_y_axis], key="secondary_y_axis_multiselect")
-                else:
-                    primary_y_axis = y_axis
+            use_secondary_y = st.checkbox("第2軸（右側Y軸）を使用する", key="use_secondary_y_checkbox", value=st.session_state.get('use_secondary_y', False))
+
+            if use_secondary_y:
+                st.session_state['use_secondary_y'] = use_secondary_y  # session_state に反映
+
+                primary_y_axis = st.multiselect(
+                    "第1軸（左側Y軸）を選択", 
+                    y_axis, 
+                    default=st.session_state.get('primary_y_axis', []),
+                    key="primary_y_axis_multiselect"
+                )
+                st.session_state['primary_y_axis'] = primary_y_axis  # session_state に反映
+
+                secondary_y_axis = st.multiselect(
+                    "第2軸（右側Y軸）を選択", 
+                    [col for col in y_axis if col not in primary_y_axis], 
+                    default=st.session_state.get('secondary_y_axis', []),
+                    key="secondary_y_axis_multiselect"
+                )
+                st.session_state['secondary_y_axis'] = secondary_y_axis  # session_state に反映
             else:
                 primary_y_axis = y_axis
+                secondary_y_axis = st.session_state.get('secondary_y_axis', [])
+                st.session_state['primary_y_axis'] = primary_y_axis  # session_state に反映
+
 
             if customize_title:
                 customize_graph_title = st.checkbox("グラフのタイトルをカスタマイズする", value=(st.session_state['graph_title'] != ''), key="customize_graph_title_checkbox")
@@ -118,19 +168,19 @@ def app():
                     st.session_state['graph_title'] = st.text_input("グラフのタイトル", value=st.session_state['graph_title'], key="graph_title_input")
                     st.session_state['title_font_size'] = st.slider("グラフのタイトルのサイズ", 10, 40, st.session_state['title_font_size'], key="title_font_size_slider")
                 else:
-                    st.session_state['graph_title'] = ''
+                    st.session_state['graph_title'] = st.session_state['graph_title']
 
                 # X軸タイトルのカスタマイズ
                 customize_x_axis_title = st.checkbox("X軸のタイトルをカスタマイズする", value=(st.session_state['x_axis_title'] != x_axis), key="customize_x_axis_title_checkbox")
                 if customize_x_axis_title:
                     st.session_state['x_axis_title'] = st.text_input("X軸のタイトル", value=st.session_state['x_axis_title'], key="x_axis_title_input")
                 else:
-                    st.session_state['x_axis_title'] = x_axis
+                    st.session_state['x_axis_title'] = st.session_state['x_axis_title']
 
                 # Y軸タイトルのカスタマイズ
                 customize_y_axis_title = st.checkbox("Y軸のタイトルをカスタマイズする", value=(st.session_state['y_axis_title'] != y_axis), key="customize_y_axis_title_checkbox")
                 if customize_y_axis_title:
-                    st.session_state['y_axis_title'] = st.text_input("第1軸（左側Y軸）のタイトル", value=st.session_state['y_axis_title'], key="y_axis_title_input")
+                    st.session_state['y_axis_title'] = st.text_input("第1軸（左側Y軸）のタイトル", value=st.session_state.get('y_axis_title', ''), key="y_axis_title_input")
                     if use_secondary_y:
                         st.session_state['secondary_y_axis_title'] = st.text_input("第2軸（右側Y軸）のタイトル", value=st.session_state.get('secondary_y_axis_title', ''), key="secondary_y_axis_title_input")
                 else:
@@ -142,19 +192,14 @@ def app():
                 st.session_state['y_axis_title_font_size'] = st.slider("Y軸のタイトルのサイズ", 10, 30, st.session_state['y_axis_title_font_size'], key="y_axis_title_font_size_slider")
                 st.session_state['x_axis_value_size'] = st.slider("X軸の値のサイズ", 10, 30, st.session_state['x_axis_value_size'], key="x_axis_value_size_slider")
                 st.session_state['y_axis_value_size'] = st.slider("Y軸の値のサイズ", 10, 30, st.session_state['y_axis_value_size'], key="y_axis_value_size_slider")
-            else:
-                st.session_state['graph_title'] = ''
-                st.session_state['x_axis_title'] = x_axis
-                st.session_state['y_axis_title'] = ', '.join(primary_y_axis)
-                if use_secondary_y:
-                    st.session_state['secondary_y_axis_title'] = ', '.join(secondary_y_axis)
 
             st.session_state['customize_title'] = customize_title
 
             # 設定を保存する辞書
             settings = {
                 'x_axis': x_axis,
-                'y_axis': primary_y_axis,
+                'y_axis': y_axis,
+                'primary_y_axis': primary_y_axis,
                 'secondary_y_axis': secondary_y_axis,
                 'use_secondary_y': use_secondary_y,  # この部分を追加
                 'graph_title': st.session_state['graph_title'],
@@ -168,9 +213,40 @@ def app():
                 'y_axis_value_size': st.session_state['y_axis_value_size']
             }
 
+            col1, col2, col3 = st.columns(3)
             # 設定の保存ボタン
-            if st.button("設定を保存"):
-                save_user_settings(settings)
+            with col1:
+                if st.button("ユーザー設定を保存", key='button_save'):
+                    # 現在の設定をすべて取得し、session_stateに保存
+                    st.session_state['settings'] = {
+                        'x_axis': x_axis,
+                        'y_axis': y_axis,
+                        'primary_y_axis': st.session_state.get('primary_y_axis', []),
+                        'secondary_y_axis': st.session_state.get('secondary_y_axis', []),
+                        'use_secondary_y': use_secondary_y,
+                        'graph_title': st.session_state.get('graph_title', ''),
+                        'title_font_size': st.session_state.get('title_font_size', 20),
+                        'x_axis_title': st.session_state.get('x_axis_title', ''),
+                        'y_axis_title': st.session_state.get('y_axis_title', ''),
+                        'secondary_y_axis_title': st.session_state.get('secondary_y_axis_title', ''),
+                        'x_axis_title_font_size': st.session_state.get('x_axis_title_font_size', 12),
+                        'y_axis_title_font_size': st.session_state.get('y_axis_title_font_size', 12),
+                        'x_axis_value_size': st.session_state.get('x_axis_value_size', 12),
+                        'y_axis_value_size': st.session_state.get('y_axis_value_size', 12)
+                    }
+                    save_user_settings(settings)
+                    
+            # 設定のリセットボタン
+            with col2:
+                if st.button("デフォルト設定を読み込み", key='button_reset'):
+                    # st.session_state をデフォルト設定にする。
+                    reset_session_state_to_default(config)
+                    st.rerun()  
+            # 設定を再読み込み
+            with col3:
+                if st.button("ユーザー設定を再読み込み", key='button_reload'):
+                    apply_user_settings(user_settings)
+                    st.rerun()
 
             fig, graph_title = create_plot(chart_type, df, settings)
             if fig:
@@ -208,4 +284,5 @@ def app():
         - ファイルのアップロードは、ローカル環境でのみ動作します。
         """)
 
-
+    with tab3:
+        st.write(st.session_state)
--- modules/state_manager.py
+++ modules/state_manager.py
@@ -5,3 +5,27 @@ def initialize_session_state(keys_defaults):
     for key, default_value in keys_defaults.items():
         if key not in st.session_state:
             st.session_state[key] = default_value
+        
+def reset_session_state_to_default(config):
+    """
+    Reset st.session_state values to the default settings provided in the config.
+
+    Args:
+    config (dict): The configuration dictionary containing default settings.
+    """
+    default_settings = config.get('default_settings', {})
+    
+    # Iterate over the default settings and set them in st.session_state
+    for key, value in default_settings.items():
+        st.session_state[key] = value
+
+def apply_user_settings(user_settings):
+    """
+    Update st.session_state values with user_settings values.
+
+    Args:
+    user_settings (dict): The dictionary containing user settings.
+    """
+    # Iterate over the user settings and set them in st.session_state
+    for key, value in user_settings.items():
+        st.session_state[key] = value
